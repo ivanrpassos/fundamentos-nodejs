f6316a6922fc0cf1905e5b45fbc7c90ab8189652
Ivan Passos <ivanpassosfilho@gmail.com>
2023-08-02 18:26:20 -0300

    Aula 8 - Aplicando Streams no módulo HTTP

---
+++ streams/fake-upload-to-http-stream.js
@@ -0,0 +1,24 @@
+import { Readable } from 'node:stream';
+
+class OneHundredStream extends Readable {
+  index = 1;
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buff = Buffer.from(String(i));
+
+        this.push(buff);
+      }
+    });
+  }
+}
+
+fetch('http://localhost:3334', {
+  method: 'POST',
+  body: new OneHundredStream(),
+  duplex: 'half',
+});
